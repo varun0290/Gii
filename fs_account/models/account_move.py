5401af6cb8893c625be77f8da03c51c6f8280155
Livbuzz <crp@fidobe.com>
2024-03-01 10:40:45 +0530

    [FIX] fixed issue of expense

--- fs_account/models/account_move.py
+++ fs_account/models/account_move.py
@@ -11,7 +11,11 @@ class AccountMove(models.Model):
         inv_analytic_account_id = self.invoice_line_ids.filtered(
             lambda l: not l.analytic_account_id
         )
-        if self.move_type != "entry" and inv_analytic_account_id:
+        if (
+            not self.expense_sheet_id
+            and self.move_type != "entry"
+            and inv_analytic_account_id
+        ):
             raise ValidationError(_("Please add analytic account in line."))
 
         return super(AccountMove, self).action_post()
