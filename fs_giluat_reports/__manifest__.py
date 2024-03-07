12259925b7f4996d5fea18097b672ab224b38b01
Harpritsinh Sisodiya <hapr@fidobe.com>
2024-03-07 10:31:46 +0530

    [ADD]: add new module fs_giluat_reports

---
+++ fs_giluat_reports/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Gulf Investment Reports",
+    "version": "17.0",
+    "author": "Harprit",
+    "summary": "Gulf Investment Reports",
+    "website": "www.livbuzz.com",
+    "license": "AGPL-3",
+    "depends": ['hr'],
+    "category": "Other",
+    "data": [
+        "report/report_noc_business_trip.xml",
+        "report/report_employement_certificate.xml",
+        "report/report_employement_offer_letter.xml",
+        "report/report_probation_period_review_confirmed.xml",
+        "report/report_probation_period_review_not_confirmed.xml",
+        "report/report_employment_certificate_salary_transfer_letter.xml",
+        "report/report_comman.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
