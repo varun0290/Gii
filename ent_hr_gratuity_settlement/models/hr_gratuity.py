6c23a788f0f6316b371d6ed8ee2ddac5534fa492
Livbuzz <crp@fidobe.com>
2024-03-22 12:50:37 +0530

    [IMP] fix issue of gratuity

--- ent_hr_gratuity_settlement/models/hr_gratuity.py
+++ ent_hr_gratuity_settlement/models/hr_gratuity.py
@@ -175,7 +175,7 @@ class EmployeeGratuity(models.Model):
 
             if hr_contract_id.date_end:
                 self.employee_contract_type = "limited"
-                employee_working_days = (hr_contract_id.date_end - joining_date).days
+                employee_working_days = (self.end_date - joining_date).days
                 self.total_working_years = employee_working_days / 365
                 self.employee_probation_years = employee_probation_days / 365
                 employee_gratuity_years = (
