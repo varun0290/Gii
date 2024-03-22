6a3b04ddd437f9d100d53cd69d3b83a7d921640c
Livbuzz <crp@fidobe.com>
2024-03-22 13:22:34 +0530

    [IMP] fix issue of gratuity

--- ent_hr_gratuity_settlement/models/hr_gratuity.py
+++ ent_hr_gratuity_settlement/models/hr_gratuity.py
@@ -179,7 +179,7 @@ class EmployeeGratuity(models.Model):
                 self.total_working_years = employee_working_days / 365
                 self.employee_probation_years = employee_probation_days / 365
                 employee_gratuity_years = (
-                    employee_working_days - employee_probation_days
+                    (employee_working_days + 1) - employee_probation_days
                 ) / 365
                 self.employee_gratuity_years = employee_gratuity_years
             else:
@@ -188,7 +188,7 @@ class EmployeeGratuity(models.Model):
                 self.total_working_years = employee_working_days / 365
                 self.employee_probation_years = employee_probation_days / 365
                 employee_gratuity_years = (
-                    employee_working_days - employee_probation_days
+                    (employee_working_days + 1) - employee_probation_days
                 ) / 365
                 self.employee_gratuity_years = round(employee_gratuity_years, 2)
 
