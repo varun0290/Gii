ad9364ecb63e6dbff7fee157e11b147f10d2c0bb
Harpritsinh Sisodiya <hapr@fidobe.com>
2024-04-22 11:51:24 +0530

    [IMP]: imp code

--- fs_account/models/account_budget.py
+++ fs_account/models/account_budget.py
@@ -12,3 +12,17 @@ class CrossoveredBudgetLines(models.Model):
     def _compute_balance(self):
         for line in self:
             line.balance = line.planned_amount + line.practical_amount
+
+class CrossoveredBudget(models.Model):
+    _inherit = "crossovered.budget"
+
+    total_balance = fields.Float(compute="_compute_totals", string="Total Balance")
+    total_planned_amount = fields.Float(compute="_compute_totals", string="Total Planned Amount")
+
+    def _compute_totals(self):
+        for cb in self:
+            cb.total_balance = 0.0
+            cb.total_planned_amount = 0.0
+            for line in cb.crossovered_budget_line:
+                cb.total_balance += line.balance
+                cb.total_planned_amount += line.planned_amount
