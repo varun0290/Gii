ee57e5b9071d05f714c44bccd24d6e86beb678cb
Livbuzz <crp@fidobe.com>
2024-06-13 11:03:04 +0530

    [FIX] fixed issue

--- fs_account/models/account_move.py
+++ fs_account/models/account_move.py
@@ -9,11 +9,12 @@ class AccountMove(models.Model):
 
     def action_post(self):
         for line in self.invoice_line_ids:
-            for account, distribution in line.analytic_distribution.items():
-                analytic_account_id = self.env["account.analytic.account"].search(
-                    [("id", "=", int(account))], limit=1
-                )
-                line.analytic_account_id = analytic_account_id.id
+            if line.analytic_distribution:
+                for account, distribution in line.analytic_distribution.items():
+                    analytic_account_id = self.env["account.analytic.account"].search(
+                        [("id", "=", int(account))], limit=1
+                    )
+                    line.analytic_account_id = analytic_account_id.id
         inv_analytic_account_id = self.invoice_line_ids.filtered(
             lambda l: not l.analytic_account_id
         )
@@ -50,6 +51,7 @@ class AccountMove(models.Model):
                 limit=1,
             )
         return journal
+
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
