f31768c3ede2b606aa517c56534bf0b740786ddb
Support-Fidobe <crp@fidobe.com>
2026-02-11 13:53:09 +0530

    [IMP] did change to show zero allocation leave in dashboard

--- fs_hr_leave/models/hr_leave_allocation.py
+++ fs_hr_leave/models/hr_leave_allocation.py
@@ -5,13 +5,13 @@ from odoo.exceptions import UserError
 class HolidaysAllocation(models.Model):
     _inherit = 'hr.leave.allocation'
 
-    @api.constrains('holiday_status_id', 'number_of_days_display', 'date_from', 'date_to')
+    @api.onchange('holiday_status_id', 'number_of_days_display', 'date_from', 'date_to')
     def fs_check_zero_allocation(self):
         for record in self:
             # Skip validation if zero allocation is allowed
             if record.holiday_status_id.allow_zero_allocation:
                 return
 
-        # Check for zero or negative allocation
-        if record.number_of_days_display <= 0:
-            raise UserError(_("Zero allocation is not allowed"))
+            # Check for zero or negative allocation
+            if record.number_of_days_display <= 0:
+                raise UserError(_("Zero allocation is not allowed"))
